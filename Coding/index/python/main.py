1c43401529bb281c8175ace301dc2e4271fd40de
HassanPortfolio <125591777+HassanPortfolio@users.noreply.github.com>
2023-08-04 00:44:40 +0500

    Initial commit

---
+++ Coding/index/python/main.py
@@ -0,0 +1,18 @@
+# print("Enter the Binary Number: ", end="")
+# bnum = input()
+
+# b = int(bnum, 2)
+# hdnum = hex(b)
+# print("\nEquivalent Hexadecimal Value = ", hdnum)
+
+
+
+# for i in range(1,3):
+#     for j in range(3):
+#        print("i= %d, j=%d" % (i,j))
+
+
+def adder (n) :
+    y= 0* 100 +n +1
+    print(y)
+    return (y)
